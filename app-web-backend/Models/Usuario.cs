580499eb85066763be43e62f6ffe477649f4b26d
leonardobalves <leonardobuckalves@gmail.com>
2023-04-18 22:06:36 -0300

    feat: Criando model usuário

---
+++ app-web-backend/Models/Usuario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace app_web_backend.Models
+{
+    [Table("Usuarios")]
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o nome!")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar a senha!")]
+        public string Senha { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o perfil!")]
+        public Perfil Perfil { get; set; }
+    }
+
+    public enum Perfil
+    {
+        Admin,
+        User
+    }
+}
